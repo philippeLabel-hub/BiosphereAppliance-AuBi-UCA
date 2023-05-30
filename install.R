1c78d928c3decbccbd9289e6e9a9e4fe2a50c17d
Philippe Label <66627318+philippeLabel-hub@users.noreply.github.com>
2023-05-30 17:07:35 +0200

    Initial commit

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # Installation of standard R packages
-install.packages(c('phangorn', 'plotly', 'corrplot', 'picante', 'vegan', 'scales', 'gplots', 'ggplot2', 'permute', 'dplyr', 'tibble', 'ape', 'RColorBrewer', 'reshape2', 'FSA', 'gridExtra', 'devtools', 'factoextra', 'shape', 'adespatial', 'betapart', 'ecodist', 'fpc', 'here', 'modEvA', 'renv', 'treemap', 'treemapify', 'GUniFrac', 'ggpmisc'))
+install.packages(c('ggplot2'))
 
 # Installation of packages with BiocManager
-BiocManager::install(c('ANCOMBC', 'Biobase', 'ComplexHeatmap', 'dada2', 'DECIPHER', 'DESeq2', 'phyloseq', 'microbiome', 'microbiomeMarker', 'MicrobiotaProcess', 'PCAtools'), ask = FALSE)
+BiocManager::install(c('PCAtools'), ask = FALSE)
